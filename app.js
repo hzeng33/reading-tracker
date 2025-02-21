6325ce5effa872d5dd856e2fe325bd63b99ff364
Hannah Zeng <hzeng9310@gmail.com>
2025-02-20 20:14:40 -0500

    Finished setting up the user model and error handlers.

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("This is the server.");
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+
+//connectDB
+const connectDB = require("./db/connect");
+
+const authenticateUser = require("./middleware/authentication");
+
+//routers
+const authRouter = require("./routes/auth");
+const booksRouter = require("./routes/books");
+
+//error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Reading tracker app.");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/books", authenticateUser, booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
